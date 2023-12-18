9cd74bb511ec50594df6e792bf96cefc4cc91ed9
ha1yu <ha1yu@gmail.com>
2023-12-18 14:40:44 +0800

    init

---
+++ src/main/java/com/test/CodeTestSrcApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTestSrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeTestSrcApplication.class, args);
+    }
+
+}
