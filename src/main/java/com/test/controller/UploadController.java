0e7733616769b25d207fd5d8710e1e6a9ed19cd0
ha1yu <ha1yu@gmail.com>
2023-12-18 15:01:58 +0800

    更新文件上传案例

--- src/main/java/com/test/controller/UploadController.java
+++ src/main/java/com/test/controller/UploadController.java
@@ -1,12 +1,20 @@
 package com.test.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("upload")
+@Controller
+@RequestMapping("/upload")
 public class UploadController {
+
+
+
+
+
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
