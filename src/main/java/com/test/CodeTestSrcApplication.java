e67d8c6e0ca7530023e40cc5d7a299f6a3e719f2
Mechoy <mechoy@yeah.net>
2023-12-19 14:40:07 +0800

    update all sql

--- src/main/java/com/test/CodeTestSrcApplication.java
+++ src/main/java/com/test/CodeTestSrcApplication.java
@@ -1,9 +1,12 @@
 package com.test;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.test.mapper")
 public class CodeTestSrcApplication {
 
     public static void main(String[] args) {
