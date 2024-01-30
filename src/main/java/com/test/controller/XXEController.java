ecf88335893cf10b3711d6e8f4876aa95b85d17d
小西巴 <c3p0ooo@xiba.com>
2024-01-30 17:18:28 +0800

    添加xxe靶场

--- src/main/java/com/test/controller/XXEController.java
+++ src/main/java/com/test/controller/XXEController.java
@@ -1,30 +1,34 @@
 package com.test.controller;
 
+import com.test.vo.XXEVo;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.xml.sax.InputSource;
 
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 
+@Controller
 @RequestMapping("/xxe")
 public class XXEController extends HttpServlet {
-    @RequestMapping
+    @RequestMapping("")
     public String index(){
         return "xxe";
     }
 
     @PostMapping("/demo1")
     @ResponseBody
-    public String demo1(@RequestBody String s) throws DocumentException, IOException {
+    public XXEVo demo1(@RequestBody String s) throws DocumentException, IOException {
+        System.out.println(s);
         SAXReader saxReader = new SAXReader();
         Document read = saxReader.read(new InputSource(new ByteArrayInputStream(s.getBytes("utf-8"))));
-        return read.getRootElement().getStringValue();
+        XXEVo xxeVo = new XXEVo();
+        xxeVo.setDocumentText(read.getRootElement().getStringValue());
+        return xxeVo;
     }
 }
