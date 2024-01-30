0904060bff8405c72395f444890de9c1a85dc1a5
小西巴 <c3p0ooo@xiba.com>
2024-01-30 16:37:18 +0800

    添加xxe靶场

--- src/main/java/com/test/controller/XXEController.java
+++ src/main/java/com/test/controller/XXEController.java
@@ -3,10 +3,7 @@ package com.test.controller;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.xml.sax.InputSource;
 
 import javax.servlet.http.HttpServlet;
@@ -15,11 +12,16 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-@RestController
+
 @RequestMapping("/xxe")
 public class XXEController extends HttpServlet {
+    @RequestMapping
+    public String index(){
+        return "xxe";
+    }
 
     @PostMapping("/demo1")
+    @ResponseBody
     public String demo1(@RequestBody String s) throws DocumentException, IOException {
         SAXReader saxReader = new SAXReader();
         Document read = saxReader.read(new InputSource(new ByteArrayInputStream(s.getBytes("utf-8"))));
